6141d3732a7a5d73f599b723c217a510cdeacd94
yada2x <lawrenceltnguyen@gmail.com>
2024-02-07 14:03:49 +1100

    Add basic video processing

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,35 @@
-import express from 'express'
+import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.send("dookie");
+})
+
+app.post('/process-video', (req, res) => {
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+    
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad Request: Missing file path.");
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360")
+        .on("end", () => {
+            res.status(200).send("Video processing finished successfully.");
+        })
+        .on("error", (err) => {
+            console.log(`An error occured: ${err.message}`);
+            res.status(500).send(`Internal Server Error: ${err.message}`);
+        })
+        .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
